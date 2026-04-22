cfc505770ea5ae4d0a57f070328eee07e5fe70b6
GregRay-skillable <greg.ray@skillable.com>
2026-04-22 17:34:01 -0500

    Update 2.FAQ_Embeddings_Table.sql
    test1

--- 2.FAQ_Embeddings_Table.sql
+++ 2.FAQ_Embeddings_Table.sql
@@ -1,35 +1,28 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+
 IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[FAQ_Embeddings]'))
 BEGIN
-	CREATE TABLE [dbo].[FAQ_Embeddings](
-		[faq_id] [int] NOT NULL,
-		[question_embedding] [vector](1536, float32) NOT NULL
-	) ON [PRIMARY]
-	GO
-	ALTER TABLE [dbo].[FAQ_Embeddings] ADD PRIMARY KEY CLUSTERED 
-	(
-		[faq_id] ASC
-	)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-	GO
-	ALTER TABLE [dbo].[FAQ_Embeddings]  WITH CHECK ADD  CONSTRAINT [FK_FAQ_Embeddings_Content] FOREIGN KEY([faq_id])
-	REFERENCES [dbo].[FAQ_Content] ([faq_id])
-	GO
-	ALTER TABLE [dbo].[FAQ_Embeddings] CHECK CONSTRAINT [FK_FAQ_Embeddings_Content]
-	GO
-	SET ANSI_PADDING ON
-	GO
-END
-GO
+    CREATE TABLE [dbo].[FAQ_Embeddings](
+        [faq_id] [int] NOT NULL,
+        [question_embedding] [vector](1536, float32) NOT NULL,
+        CONSTRAINT [PK_FAQ_Embeddings] PRIMARY KEY CLUSTERED
+        (
+            [faq_id] ASC
+        ),
+        CONSTRAINT [FK_FAQ_Embeddings_Content] FOREIGN KEY ([faq_id])
+            REFERENCES [dbo].[FAQ_Content] ([faq_id])
+    );
+END;
 
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_FAQ_Embedding' AND object_id = OBJECT_ID(N'[dbo].[FAQ_Embeddings]'))
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.indexes
+    WHERE name = N'IX_FAQ_Embedding'
+      AND object_id = OBJECT_ID(N'[dbo].[FAQ_Embeddings]')
+)
 BEGIN
-	CREATE VECTOR INDEX [IX_FAQ_Embedding] ON [dbo].[FAQ_Embeddings]
-	(
-		[question_embedding]
-	)WITH (METRIC = 'COSINE', TYPE = 'DiskANN') ON [PRIMARY]
-	GO
-END
-GO
+    CREATE VECTOR INDEX [IX_FAQ_Embedding]
+    ON [dbo].[FAQ_Embeddings] ([question_embedding])
+    WITH (METRIC = 'COSINE', TYPE = 'DiskANN');
+END;
