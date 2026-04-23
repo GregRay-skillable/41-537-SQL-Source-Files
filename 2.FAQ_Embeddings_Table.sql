5f823633638200bed0255be571a05693007cb8b4
GregRay-skillable <greg.ray@skillable.com>
2026-04-22 21:07:47 -0500

    Update 2.FAQ_Embeddings_Table.sql

--- 2.FAQ_Embeddings_Table.sql
+++ 2.FAQ_Embeddings_Table.sql
@@ -1,28 +1,14 @@
 SET ANSI_NULLS ON;
 SET QUOTED_IDENTIFIER ON;
 
-IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[FAQ_Embeddings]'))
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE object_id = OBJECT_ID(N'dbo.FAQ_Embeddings'))
 BEGIN
-    CREATE TABLE [dbo].[FAQ_Embeddings](
-        [faq_id] [int] NOT NULL,
-        [question_embedding] [vector](1536, float32) NOT NULL,
-        CONSTRAINT [PK_FAQ_Embeddings] PRIMARY KEY CLUSTERED
-        (
-            [faq_id] ASC
-        ),
-        CONSTRAINT [FK_FAQ_Embeddings_Content] FOREIGN KEY ([faq_id])
-            REFERENCES [dbo].[FAQ_Content] ([faq_id])
+    CREATE TABLE dbo.FAQ_Embeddings
+    (
+        faq_id INT NOT NULL,
+        question_embedding VECTOR(1536) NOT NULL,
+        CONSTRAINT PK_FAQ_Embeddings PRIMARY KEY CLUSTERED (faq_id ASC),
+        CONSTRAINT FK_FAQ_Embeddings_Content FOREIGN KEY (faq_id)
+            REFERENCES dbo.FAQ_Content (faq_id)
     );
-END;
-
-IF NOT EXISTS (
-    SELECT 1
-    FROM sys.indexes
-    WHERE name = N'IX_FAQ_Embedding'
-      AND object_id = OBJECT_ID(N'[dbo].[FAQ_Embeddings]')
-)
-BEGIN
-    CREATE VECTOR INDEX [IX_FAQ_Embedding]
-    ON [dbo].[FAQ_Embeddings] ([question_embedding])
-    WITH (METRIC = 'COSINE', TYPE = 'DiskANN');
 END;
