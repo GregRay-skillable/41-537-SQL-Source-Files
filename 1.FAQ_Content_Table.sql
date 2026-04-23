8e29011eaae51bdfd9a3d2275189a685b26f626e
GregRay-skillable <greg.ray@skillable.com>
2026-04-22 21:03:40 -0500

    Update 1.FAQ_Content_Table.sql

--- 1.FAQ_Content_Table.sql
+++ 1.FAQ_Content_Table.sql
@@ -1,24 +1,15 @@
-IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[FAQ_Content]'))
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE object_id = OBJECT_ID(N'dbo.FAQ_Content'))
 BEGIN
-    SET ANSI_NULLS ON;
-    SET QUOTED_IDENTIFIER ON;
-
-    CREATE TABLE [dbo].[FAQ_Content](
-        [faq_id] [int] IDENTITY(1,1) NOT NULL,
-        [category]  NOT NULL,
-        [question]  NOT NULL,
-        [answer] [nvarchar](max) NOT NULL,
-        [keywords]  NULL,
-        [is_active] [bit] NOT NULL,
-        [created_at]  NOT NULL,
-        [updated_at]  NOT NULL,
-        CONSTRAINT [PK_FAQ_Content] PRIMARY KEY CLUSTERED
-        (
-            [faq_id] ASC
-        )
+    CREATE TABLE dbo.FAQ_Content
+    (
+        faq_id INT IDENTITY(1,1) NOT NULL,
+        category NVARCHAR(100) NOT NULL,
+        question NVARCHAR(1000) NOT NULL,
+        answer NVARCHAR(MAX) NOT NULL,
+        keywords NVARCHAR(500) NULL,
+        is_active BIT NOT NULL CONSTRAINT DF_FAQ_Content_is_active DEFAULT (1),
+        created_at DATETIME2(6) NOT NULL CONSTRAINT DF_FAQ_Content_created_at DEFAULT (sysutcdatetime()),
+        updated_at DATETIME2(6) NOT NULL CONSTRAINT DF_FAQ_Content_updated_at DEFAULT (sysutcdatetime()),
+        CONSTRAINT PK_FAQ_Content PRIMARY KEY CLUSTERED (faq_id ASC)
     );
-
-    ALTER TABLE [dbo].[FAQ_Content] ADD DEFAULT ((1)) FOR [is_active];
-    ALTER TABLE [dbo].[FAQ_Content] ADD DEFAULT (sysutcdatetime()) FOR [created_at];
-    ALTER TABLE [dbo].[FAQ_Content] ADD DEFAULT (sysutcdatetime()) FOR [updated_at];
 END;
